691a73c9ea588a15227d297d8e68fb7adf10d2e8
minec-c <reciprocalidea@gmail.com>
2026-02-07 16:57:05 -0400

    Update Loader.gd

--- template/scripts/Loader.gd
+++ template/scripts/Loader.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 func _ready():
-	var path = "res://user_scripts/thisgdscript.gd"
-	var f = File.new()
-	if f.file_exists(path):
-		var s = load(path)
-		set_script(s)
-	else:
-		print("User script nof found")
+#	var path = "res://user_scripts/thisgdscript.gd"
+#	var f = File.new()
+#	if f.file_exists(path):
+#		var s = load(path)
+#		set_script(s)
+#	else:
+#		print("User script nof found")
+
+# FORCE Godot to register the script at export time
+const UserScript = preload("res://user_scripts/thisgdscript.gd")
+
+func _ready():
+	var instance = UserScript.new()
+	if instance.has_method("run"):
+		instance.run(self)
